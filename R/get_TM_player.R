fb94ad5e42c890df1bfcde0db52f87e4bb71a112
Domenic Di Francesco <domenicdifrancesco@gmail.com>
2022-08-01 11:06:22 +0100

    setting_up

---
+++ R/get_TM_player.R
@@ -0,0 +1,97 @@
+#' get_TM_player
+#'
+#' A webscraping function for the website 'https://www.transfermarkt.com/'.
+#' Returns a preprocessed tibble of a player.
+#'
+#'
+#' @param playerurl A valid transfermarkt url. Navigate to a player's page, and then copy the url - REQUIRED.
+#' @param user_agent A character introducing yourself to the website - REQUIRED.
+#' @param raw  Logical: if TRUE, returns the unprocessed table. if FALSE, returns a processed tibble - defaults to FALSE
+#' @keywords Webscraping, Football, Soccer
+#' @export
+#' @examples
+#' dusan_vlahovic <- get_TM_player(player_url = 'https://www.transfermarkt.co.uk/du-scaron-an-vlahović/profil/spieler/357498', user_agent = 'John Smith personal project')
+#' Blackburn_Rovers_squad <- get_TM_squad(url = 'https://www.transfermarkt.co.uk/blackburn-rovers/startseite/verein/164', user_agent = 'John Smith personal project')
+
+get_TM_player <- function(player_url, user_agent, raw = FALSE){
+
+  require(tidyverse); require(polite)
+
+  player_url <- (Juve_squad |> filter(player == 'Dušan Vlahović'))$player_url
+
+  url <- player_url |> gsub(pattern = 'profil', replacement = 'leistungsdatendetails') |>
+    paste0('/saison//verein/0/liga/0/wettbewerb//pos/0/trainer_id/0/plus/1')
+
+  session <- polite::bow(url = url, user_agent = user_agent)
+
+  raw_player <- session |>
+    polite::scrape() |>
+    rvest::html_nodes('#yw1 > table') |>
+    rvest::html_table(fill = TRUE)
+
+  page_urls <- session |>
+    polite::scrape() |>
+    rvest::html_nodes('#yw1 > table') |>
+    rvest::html_nodes('td') |>
+    rvest::html_nodes(css = 'a') |>
+    rvest::html_attr('href')
+
+  club_urls <- page_urls[grepl(pattern = '/startseite/verein/', x = page_urls)]
+
+  get_team_name <- function(club_url){
+
+    polite::bow(url = club_url, user_agent = user_agent) |>
+      polite::scrape() |>
+      rvest::html_nodes('#verein_head > div > div.dataHeader.dataExtended > div.dataMain > div > div.dataName > h1') |>
+      rvest::html_text() |>
+      stringr::str_replace_all(pattern = '\n', replacement = '') |>
+      stringr::str_trim()
+
+  }
+
+  clubs <- purrr::map_chr(.x = club_urls,
+                          .f = function(.x) {get_team_name(club_url = paste0('https://www.transfermarkt.co.uk', .x))})
+
+  cols <- c('season', NA, 'competition', 'team', NA, 'apps', 'points_per_game', 'goals', 'assists', 'own_goals',
+            'subbed_on', 'subbed_off', 'yellow_cards', 'second_yellows', 'straight_reds', NA, NA, 'mins_played')
+
+  player <- raw_player |>
+    as.data.frame() |> as_tibble() |>
+    dplyr::filter(Season != "") |>
+    dplyr::mutate(dplyr::across(.cols = everything(),
+                                .fns = ~ stringr::str_replace_all(string = ., pattern = '-', replacement = '0')))
+
+  colnames(player) <- cols; player <- player[!is.na(names(player))]
+
+  player <- player |>
+    dplyr::mutate(team = clubs,
+                  season = forcats::as_factor(x = season),
+                  competition = forcats::as_factor(x = competition),
+                  team = forcats::as_factor(x = team),
+                  apps = as.integer(x = apps),
+                  points_per_game = as.double(x = points_per_game),
+                  goals = as.integer(x = goals),
+                  assists = as.integer(x = assists),
+                  own_goals = as.integer(x = own_goals),
+                  subbed_on = as.integer(x = subbed_on),
+                  subbed_off = as.integer(x = subbed_off),
+                  yellow_cards = as.integer(x = yellow_cards),
+                  second_yellows = as.integer(x = second_yellows),
+                  straight_reds = as.integer(x = straight_reds),
+                  mins_played = stringr::str_remove(string = mins_played, pattern = "'"),
+                  mins_played = as.double(x = mins_played),
+                  mins_played = dplyr::case_when(
+                    mins_played %% 1 != 0 ~ mins_played * 1e3,
+                    T ~ mins_played
+                  ),
+                  red_cards = second_yellows + straight_reds
+
+    )
+
+  if(raw == FALSE){
+    return(player)
+  } else {
+    return(raw_player)
+  }
+
+}
---
+++ R/get_TM_squad.R
@@ -0,0 +1,121 @@
+#' get_TM_squad
+#'
+#' A webscraping function for the website 'https://www.transfermarkt.com/'.
+#' Returns a preprocessed tibble of any squad.
+#'
+#'
+#' @param url A valid ()detailed) transfermarkt url. Navigate to a club page, click on the 'detailed' tab, and then paste the url - REQUIRED.
+#' @param user_agent A character introducing yourself to the website - REQUIRED.
+#' @param raw  Logical: if TRUE, returns the unprocessed table. if FALSE, returns a processed tibble - defaults to FALSE
+#' @param year Numeric: specify the year for which you would like the data
+#' @keywords Webscraping, Football, Soccer
+#' @export
+#' @examples
+#' Juventus squad <- get_TM_squad(squad_url = 'https://www.transfermarkt.co.uk/juventus-turin/startseite/verein/506', user_agent = 'John Smith personal project')
+#' Blackburn_Rovers_squad <- get_TM_squad(squad_url = 'https://www.transfermarkt.co.uk/blackburn-rovers/startseite/verein/164', user_agent = 'John Smith personal project')
+
+get_TM_squad <- function(squad_url, user_agent, raw = FALSE, year = 2022){
+
+  require(tidyverse); require(polite)
+
+  url <- stringr::str_replace(string = squad_url, pattern = 'startseite', replacement = 'kader') |>
+    paste0('/saison_id/') |>
+    paste0(year) |>
+    paste0('/plus/1')
+
+  session <- polite::bow(url = url, user_agent = user_agent)
+
+  raw_squad <- session |>
+    polite::scrape() |>
+    rvest::html_nodes('#yw1 > table') |>
+    rvest::html_table(fill = TRUE)
+
+  team_name <- session |>
+    polite::scrape() |>
+    rvest::html_nodes('#verein_head > div > div.dataHeader.dataExtended > div.dataMain > div > div.dataName > h1') |>
+    rvest::html_text() |>
+    stringr::str_replace_all(pattern = '\n', replacement = '') |>
+    stringr::str_trim()
+
+  page_urls <- session |>
+    polite::scrape() |>
+    rvest::html_nodes('#yw1 > table') |>
+    rvest::html_nodes('td') |>
+    rvest::html_nodes(css = 'a') |>
+    rvest::html_attr('href')
+
+  player_urls <- page_urls[grepl(pattern = '/spieler/', x = page_urls)]
+
+  squad <- raw_squad |>
+    as.data.frame() |> tibble::as_tibble() |>
+    dplyr::mutate(X. = X. |> as.numeric()) |>
+    dplyr::filter(!is.na(Nat.))
+
+  cols <- c('number', NA, NA, 'player', 'position', 'DoB', NA, 'height_m', 'foot', 'joined', NA, 'contract_exp', 'market_value')
+  colnames(squad) <- cols; squad <- squad[!is.na(names(squad))]
+
+  squad <- squad |>
+    dplyr::mutate(currency = dplyr::case_when(
+      grepl(pattern = '£', x = market_value) ~ '£',
+      grepl(pattern = '€', x = market_value) ~ '€',
+      grepl(pattern = '$', x = market_value) ~ '$'
+    )) |>
+    dplyr::mutate(market_value = sub(pattern = '£|€|$', replacement = '', x = market_value)) |>
+    dplyr::mutate(market_value = dplyr::case_when(
+      grepl(pattern = 'm', x = market_value) ~ 1e6 * as.numeric(sub(pattern = 'm',
+                                                                        replacement = '',
+                                                                        x = market_value)),
+      grepl(pattern = 'Th.', x = market_value) ~ 1e3 * as.numeric(sub(pattern = 'Th.',
+                                                                          replacement = '',
+                                                                          x = market_value))
+    )) |>
+    dplyr::mutate(contract_exp = lubridate::mdy(gsub(pattern = ' ', replacement = '-',
+                                                     x = gsub(pattern = ',', replacement = '', x = contract_exp)))) |>
+    dplyr::mutate(joined = lubridate::mdy(gsub(pattern = ' ', replacement = '-',
+                                               x = gsub(pattern = ',', replacement = '', x = joined)))) |>
+    dplyr::mutate(height_m = as.numeric(gsub(pattern = ',', replacement = '.',
+                                             x = gsub(pattern = ' m', replacement = '', x = height_m)))) |>
+    dplyr::mutate(DoB = lubridate::mdy(gsub(pattern = ' ', replacement = '-',
+                                     x = gsub(pattern = ',', replacement = '',
+                                              x = stringr::str_replace(string = stringr::str_sub(DoB, end = -5),
+                                                                       pattern = " \\(.*",
+                                                                       replacement = ''))))) |>
+    dplyr::mutate(surname = stringr::str_trim(string = stringr::word(string =  player, sep = '[ ]', start = -1),
+                                              side = 'both'),
+                  name = stringr::word(string = player, sep = ' ', end = 1)) |>
+    dplyr::mutate(Age = lubridate::interval(start = DoB, end = Sys.Date()) / lubridate::years(1)) |>
+    dplyr::mutate(squad_value = sum(market_value)) |>
+    dplyr::mutate(player = forcats::fct_reorder(.f = as.factor(player), .x = market_value),
+                  position = forcats::as_factor(position),
+                  foot = forcats::as_factor(foot),
+                  team = team_name,
+                  player_url = purrr::map_chr(.x = player_urls,
+                                              .f = function(.x){paste0('https://www.transfermarkt.co.uk', .x)[[1]]})) |>
+    dplyr::mutate_if(is.character, list(~dplyr::na_if(.,""))) |>
+    dplyr::arrange(desc(market_value))
+
+  # squad$player_name = character(length = squad |> nrow())
+  #
+  # for (i in seq(from = 1, to = nrow(squad))){
+  #
+  #   if(grepl(pattern = squad$name[i], x = squad$surname[i])) {
+  #
+  #     squad$player_name[i] <- stringr::str_sub(string = squad$surname[i], start = 1,
+  #                                              end = nchar(squad$surname[i])/2)
+  #
+  #   } else {
+  #     squad$player_name[i] <- paste(squad$name[i], squad$surname[i])
+  #   }
+  #
+  # }
+
+  if(raw == FALSE){
+    return(squad)
+  } else {
+    return(raw_squad)
+  }
+
+}
+
+
+
